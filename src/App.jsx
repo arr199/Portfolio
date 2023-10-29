abbb7c53218e61cdd91ef49089f83c1ce093426f
seafood12 <abieledelso@gmail.com>
2023-10-29 23:28:33 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App () {
 
   return (
     <>
-
    <main className='snap-mandatory snap-y overflow-auto h-screen overflow-x-hidden w-full'>
       <Header></Header>
       <PageContext.Provider value={{ currentPage, setCurrentPage }}>
